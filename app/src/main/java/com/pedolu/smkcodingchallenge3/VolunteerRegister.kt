9969aff86f4c9cd822d6d39292d7462da3a09970
zxc1520 <afifghulam21@gmail.com>
2020-07-05 20:03:58 +0700

    add: Add register for volunteer layout

---
+++ app/src/main/java/com/pedolu/smkcodingchallenge3/VolunteerRegister.kt
@@ -0,0 +1,11 @@
+package com.pedolu.smkcodingchallenge3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VolunteerRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volunteer_register)
+    }
+}
